1f81c710d2c0adc1aaa0d4d70a8b258d7b5c5960
lixiaochen <lixiaochen@meishubao.com>
2022-08-02 09:00:17 +0800

    将要添加的用户信息用随机数处理

--- src/test/java/com/hogwarts/appium/ch08_wework/AddMemberTest.java
+++ src/test/java/com/hogwarts/appium/ch08_wework/AddMemberTest.java
@@ -2,6 +2,7 @@ package com.hogwarts.appium.ch08_wework;
 
 import com.hogwarts.appium.ch07_po.page.XueQiuAPP;
 import com.hogwarts.appium.ch08_wework.page.WeWorkMainPage;
+import com.util.FakerUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import static org.hamcrest.Matchers.is;
 
 public class AddMemberTest {
     public static WeWorkMainPage weWork;
+    public static String memberName = FakerUtil.get_name();
+    public static String memberPhone = FakerUtil.get_zh_phone();
 
     @BeforeAll
     public static void Before(){
@@ -25,7 +28,7 @@ public class AddMemberTest {
                 .switchToContactPage()// 切换到通讯录界面
                 .goToChooseContactMethodPage() // 点击添加成员进入到选择添加方式界面
                 .goToEditContactPage()
-                .addMember("小江山", "13120015358");
+                .addMember(memberName, memberPhone);
     }
 
     @Test
