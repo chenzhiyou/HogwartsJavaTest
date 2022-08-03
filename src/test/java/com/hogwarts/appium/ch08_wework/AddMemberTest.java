fca2a596b6bfc47f25543414d71b66fa8545b036
lixiaochen <lixiaochen@meishubao.com>
2022-08-03 23:27:58 +0800

    框架封装思想

--- src/test/java/com/hogwarts/appium/ch08_wework/AddMemberTest.java
+++ src/test/java/com/hogwarts/appium/ch08_wework/AddMemberTest.java
@@ -11,6 +11,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * 打造测试框架的需求与价值：
+ *      领域模型适配：封装业务实现，实现业务管理
+ *      提高效率：降低用例维护成本，提高执行效率
+ *      增强功能：解决已有框架不满足的情况
+ * 自动化框架应具备的功能：
+ *      支持管理用例，运行用例
+ *      支持查找元素/定位元素，对元素/页面进行各种操作
+ *      支持生成测试报告
+ *      能够时间功能的复用
+ *      当页面有异常弹窗的时候，可以进行有效的处理
+ *      当用例失败，需要添加失败时的日志，截图等信息放在测试报告中
+ *      多设备并发
+ *      支持平台化
+ * 项目结构：
+ *      框架层：yaml解析、日志收集、数据驱动、ini配置文件、用例数据、报告管理
+ *      基础层：基础driver封装、查找封装、操作封装、领域模型
+ *      业务层/PO层：Page封装，各个页面的PO
+ *      用例层：测试用例集、各个case
+ */
 public class AddMemberTest {
     public static WeWorkMainPage weWork;
     public static String memberName = FakerUtil.get_name();
