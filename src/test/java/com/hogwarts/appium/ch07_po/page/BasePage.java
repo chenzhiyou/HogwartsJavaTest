babdd3b3598839b489ba20137129c1c0dda2c75f
lixiaochen <lixiaochen@meishubao.com>
2022-08-04 08:26:51 +0800

    测试过程中异常弹窗的处理方法

--- src/test/java/com/hogwarts/appium/ch07_po/page/BasePage.java
+++ src/test/java/com/hogwarts/appium/ch07_po/page/BasePage.java
@@ -1,11 +1,17 @@
 package com.hogwarts.appium.ch07_po.page;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BasePage {
     public AndroidDriver driver;
+    List<By> blackList = Arrays.asList(AppiumBy.id(""),
+            AppiumBy.id(""));
 
     public BasePage(){
 
@@ -15,7 +21,19 @@ public class BasePage {
         this.driver = driver;
     }
 
+    // 添加黑名单的处理逻辑
     public WebElement find(By by){
-        return driver.findElement(by);
+        try{
+            return driver.findElement(by);
+        }catch (Exception e){
+            for (By by1 : blackList) {
+                List<WebElement> elements =driver.findElements(by1);
+               if (elements.size()>0){
+                    elements.get(0).click();
+                    return find(by);
+                }
+            }
+            throw e;
+        }
     }
 }
