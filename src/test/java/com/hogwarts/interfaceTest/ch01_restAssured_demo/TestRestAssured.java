a26223ee203b0d9fccbaf0ee823a51f9c856133f
lixiaochen <lixiaochen@meishubao.com>
2022-08-14 15:56:28 +0800

    RestAssured demo

---
+++ src/test/java/com/hogwarts/interfaceTest/ch01_restAssured_demo/TestRestAssured.java
@@ -0,0 +1,19 @@
+package com.hogwarts.interfaceTest.ch01_restAssured_demo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestRestAssured {
+
+    @Test
+    void fun(){
+        given()
+                .header("Hello", "hogwarts")
+                .when()
+                .get("https://httpbin.ceshiren.com/get")
+                .then()
+                .log()
+                .all();
+    }
+}
