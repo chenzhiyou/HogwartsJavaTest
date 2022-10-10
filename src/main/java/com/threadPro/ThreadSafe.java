f7fdce5a3c41da6f2db54518541e57dd29c8cdd9
lixiaochen <lixiaochen@meishubao.com>
2022-10-10 09:10:05 +0800

    线程安全的实现

--- src/main/java/com/threadPro/ThreadSafe.java
+++ src/main/java/com/threadPro/ThreadSafe.java
@@ -2,20 +2,40 @@ package com.threadPro;
 
 class Ticket implements Runnable{
     private int ticket = 100;
+    Object lock = new Object();
 
     @Override
     public void run() {
         while (true){// 只要有票，就一直卖
+            // 使用同步代码块解决线程安全问题
+            /**
+             * synchronized(临时资源对象){ 对临界资源对象加锁
+             *     代码(原子操作)
+             * }
+             */
+            synchronized (Ticket.class){ // 每次只会允许一个线程进来执行里面的代码 .class文件加载到内存的时候，只会创建一个Class类的对象
+                if (ticket > 0){
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+ "卖了第"+ticket--);
+                }
+            }
+
+        }
+    }
+
+    // 使用synchronized 来修饰方法， 有锁，this
+    public synchronized void sellTicket(){
+        if (ticket > 0){
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if (ticket > 0){
-                System.out.println(Thread.currentThread().getName()+ "卖了第"+ticket--);
-            }
-
-
+            System.out.println(Thread.currentThread().getName()+ "卖了第"+ticket--);
         }
     }
 }
