992be04ea4989f4d0baf7468ef13449287b977e3
lixiaochen <lixiaochen@meishubao.com>
2022-10-11 09:48:29 +0800

    预定义的注解

---
+++ src/main/java/com/enumPro/AnnotationDemo.java
@@ -0,0 +1,19 @@
+package com.enumPro;
+
+class Person2{
+    public void eat(){
+
+    }
+}
+class Student extends Person2{
+
+    @Override // 约束下面的方法声明符合重写的要求
+    public void eat(){
+
+    }
+}
+public class AnnotationDemo {
+    public static void main(String[] args) {
+
+    }
+}
