c533f94c5c5a907b99ea90a9e4f24de1c6323ef6
lixiaochen <lixiaochen@meishubao.com>
2022-10-19 09:43:00 +0800

    用户名密码登录

--- src/main/java/com/jdbcPro/LoginJDBC.java
+++ src/main/java/com/jdbcPro/LoginJDBC.java
@@ -1,9 +1,16 @@
 package com.jdbcPro;
 
 import java.sql.*;
+import java.util.Scanner;
 
-public class JDBCSelectDemo {
+public class LoginJDBC {
     public static void main(String[] args) throws SQLException {
+        // 用户在控制台录入用户名和密码，去数据库查询是否存在记录，如果存在，登录成功，否则，登录失败
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请录入用户名：");
+        String name = scanner.nextLine();
+        System.out.println("请输入密码：");
+        String pw = scanner.nextLine();
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
@@ -18,15 +25,17 @@ public class JDBCSelectDemo {
             // 获取执行SQL的对象
             statement = connection.createStatement();
             // 执行SQL并获取结果集
-            resultSet = statement.executeQuery("select * from pserson");
+            resultSet = statement.executeQuery("select * from users where username='"+name+"'");
             // 处理结果
             while (resultSet.next()){
                 // 一列一列获取
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                int age = resultSet.getInt("age");
-                String sex = resultSet.getString("sex");
-                System.out.println(id+ "-" + name + "-"+ age+ "-"+ sex);
+                String usPw = resultSet.getString("password");
+                System.out.println(usPw);
+                if (usPw.equals(pw)){
+                    System.out.println("登录成功");
+                }else {
+                    System.out.println("登录失败");
+                }
             }
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -36,4 +45,5 @@ public class JDBCSelectDemo {
             connection.close();
         }
     }
+
 }
