e24cd5c7036cd585e42abc427d3202fb83ec1bfe
lixiaochen <lixiaochen@meishubao.com>
2022-11-05 20:42:29 +0800

    cookie的demo

---
+++ src/main/java/com/cookieSessionPro/ServletCookieDemo1.java
@@ -0,0 +1,23 @@
+package com.cookieSessionPro;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "/ServletCookieDemo1", value = "/ServletCookieDemo1")
+public class ServletCookieDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 创建cookie
+        Cookie cookie = new Cookie("data", "ZhangSan");
+
+        // 通过response发送数据到客户端
+        response.addCookie(cookie);
+    }
+}
---
+++ src/main/java/com/cookieSessionPro/ServletCookieDemo2.java
@@ -0,0 +1,25 @@
+package com.cookieSessionPro;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "/ServletCookieDemo2", value = "/ServletCookieDemo2")
+public class ServletCookieDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 获取其他servlet共享的数据
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie: cookies){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println(name + ": "+ value);
+        }
+    }
+}
