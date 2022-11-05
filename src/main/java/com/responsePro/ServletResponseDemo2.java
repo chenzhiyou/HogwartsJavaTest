6e083b706fdd990dbf3e10ca7ae3b25052d10b41
lixiaochen <lixiaochen@meishubao.com>
2022-11-05 17:37:06 +0800

    解决乱码

--- src/main/java/com/responsePro/ServletResponseDemo2.java
+++ src/main/java/com/responsePro/ServletResponseDemo2.java
@@ -22,7 +22,14 @@ public class ServletResponseDemo2 extends HttpServlet {
         // 获取共享的数据
         String data = (String) request.getAttribute("data");
         System.out.println("获取Request中共享的数据" + data);
+        // 解决响应乱码问题
+        response.setCharacterEncoding("utf-8");
+        // 不推荐
+//        response.setHeader("Content-type", "text/html;charset=utf-8");
+        // 推荐使用的方式
+        response.setContentType("text/html;charset=utf-8");
         PrintWriter writer = response.getWriter();
+
         writer.write("重定向处理完毕， 返回浏览器");
     }
 }
