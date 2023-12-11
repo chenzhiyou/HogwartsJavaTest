b49d63c472ce21ea4de3c8d42833eb88bd504523
lixiaochen <lixiaochen@meishubao.com>
2023-12-11 17:40:49 +0800

    添加删除信息

--- src/main/java/learn/ch15_homework/StudentManagement.java
+++ src/main/java/learn/ch15_homework/StudentManagement.java
@@ -1,12 +1,36 @@
 package learn.ch15_homework;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StudentManagement {
+    private List<Student> students = new LinkedList<>();
+
+    public List<Student> getStudents() {
+        for (int i =0; i<students.size(); i++){
+            System.out.println(students.get(i));
+        }
+        return students;
+    }
+
+
     public void addStudent(int id, String name, String sex){
         Student student = new Student();
         student.setId(id);
         student.setSex(sex);
         student.setName(name);
-        System.out.println(student.toString());
+        students.add(student);
+    }
+
+    public void deleteStudent(int id){
+        for (Student student : students){
+            if (student.getId() == id){
+//                System.out.println("删除的学员为： " +student);
+                students.remove(student);
+            }
+        }
+        System.out.println("删除后的学员信息：");
+        getStudents();
     }
 
 }
--- src/main/java/learn/ch15_homework/StudentManagementMain.java
+++ src/main/java/learn/ch15_homework/StudentManagementMain.java
@@ -6,5 +6,8 @@ public class StudentManagementMain {
         studentManagement.addStudent(1001, "张三", "男");
         studentManagement.addStudent(1002, "莉丝", "女");
         studentManagement.addStudent(1003, "王武", "男");
+        System.out.println("添加的学员信息：");
+        studentManagement.getStudents();
+        studentManagement.deleteStudent(1002);
     }
 }
