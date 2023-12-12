337361dbc478c0f43dc9f6f6377755ddada37667
lixiaochen <lixiaochen@meishubao.com>
2023-12-13 06:27:14 +0800

    标准库之科学计算

---
+++ src/main/java/learn/ch31_matg/MathDemo.java
@@ -0,0 +1,16 @@
+package learn.ch31_matg;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println("E: "+ Math.E);
+        System.out.println("PI: "+ Math.PI);
+        System.out.println("abs绝对值：" +Math.abs(-8));
+        System.out.println("向上取整： "+ Math.ceil(3.4));
+        System.out.println("向下取整: "+ Math.floor(5.9));
+        System.out.println("四舍五入: "+ Math.round(6.6));
+        System.out.println("最大值： "+ Math.max(6,7));
+        System.out.println("最小值： "+ Math.min(6,7));
+        System.out.println("平方根： "+ Math.sqrt(16.0));
+        System.out.println("求幂： "+ Math.pow(2,3));
+    }
+}
